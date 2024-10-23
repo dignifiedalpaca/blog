15cb55b14f133cccfd1fab00d3c4787788884c88
TayzenDev <6642745-Tayzen@users.noreply.gitlab.com>
2024-10-23 23:38:51 +0000

    fix: getting back the RSS icon

--- pages/components/navbar.tsx
+++ pages/components/navbar.tsx
@@ -1,6 +1,7 @@
 /** @jsxImportSource hono/jsx */
 
 import type { FC } from "hono/jsx";
+import RssIcon from "./rss.tsx";
 
 export const Navbar: FC<{ title: string }> = (props: { title: string }) => {
     return (
@@ -12,7 +13,7 @@ export const Navbar: FC<{ title: string }> = (props: { title: string }) => {
             <ul class={"navbar-links"}>
                 <li>
                     <a href="/rss.xml" hx-boost="false">
-                        {/* <RssIcon /> RSS */}
+                        <RssIcon /> RSS
                     </a>
                 </li>
             </ul>
