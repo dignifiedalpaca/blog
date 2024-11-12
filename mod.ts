0383ab36a0825acec6aff2d2d5c1831d0e2fba12
TayzenDev <110170236+TayzenDev@users.noreply.github.com>
2024-11-12 11:20:35 +0100

    feat: v1.0.1 More documentation

--- mod.ts
+++ mod.ts
@@ -2,11 +2,20 @@ import type { SmallblogConfig } from "./types.ts";
 import { createServer } from "./server.tsx";
 import { createCli } from "./cli.ts";
 
+export type { SmallblogConfig };
+
+/**
+ * The main class of Smallblog instantiating the server and cli from your config.
+ */
 export class Smallblog {
   private server;
   private cli;
 
-  constructor(options: Partial<SmallblogConfig> = {}) {
+  /**
+   * Creates your Smallblog instance
+   * @param config The options to create your blog.
+   */
+  constructor(config: Partial<SmallblogConfig> = {}) {
     const {
       postsFolder = "posts/",
       draftsFolder = "drafts/",
@@ -21,7 +30,7 @@ export class Smallblog {
       locale,
       customHeaderScript,
       customBodyScript,
-    } = options;
+    } = config;
 
     this.server = createServer({
       postsFolder,
@@ -41,10 +50,18 @@ export class Smallblog {
     this.cli = createCli(postsFolder, draftsFolder);
   }
 
+  /**
+   * The fetch method to reoslve the HTTP requests
+   * @param req The input request
+   */
   fetch = (req: Request): Response | Promise<Response> => {
     return this.server.fetch(req);
   };
 
+  /**
+   * The method which handles the CLI
+   * @param args the arguments of the function
+   */
   run = (args: string[]): void => {
     return this.cli(args);
   };
